db7c2ce0dce14bad7d95abd0173d2d554a6ace57
Bryan <bryanriegler2@gmail.com>
2023-11-29 16:43:53 -0300

    a

--- src/daos/mongodb/models/product.model.js
+++ src/daos/mongodb/models/product.model.js
@@ -5,13 +5,13 @@ import mongoosePaginate from "mongoose-paginate-v2";
 export const productsCollection = "products";
 
 export const productsSchema = new Schema({
-    title: { type: "string", required: true },
-    description: { type: "string", required: true },
-    price: { type: "number", required: true },
-    code: { type: "string", required: true },
-    stock: { type: "number", required: true },
-    category: { type: "string", required: true },
-    status: { type: "boolean" }
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    price: { type: Number, required: true },
+    code: { type: String, required: true },
+    stock: { type: Number, required: true },
+    category: { type: String, required: true },
+    status: { type: Boolean }
 });
 
 productsSchema.plugin(mongoosePaginate)
